95df0d479cbc62c715f85bd2ce1aad637a60f8db
LSHummel <lshummel13@gmail.com>
2026-04-24 09:37:44 -0300

    menu completo

--- src/main.c
+++ src/main.c
@@ -7,7 +7,10 @@ printf("1 - Inserir notas\n");
 printf("2 - Calcular media\n");
 printf("3 - Verificar situacao\n");
 printf("4 - Exibir resultado\n");
-
+printf("5 - Calcular derivada\n");
+printf("6 - Sair\n");
+printf("Escolha uma opcao: ");
+}
 int main() {
    
 
