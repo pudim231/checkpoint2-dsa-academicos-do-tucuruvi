7fc5112781ee99b98e6ba0f5fb957e063d9b6216
LSHummel <lshummel13@gmail.com>
2026-04-24 09:35:23 -0300

    inicio do menu

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
+void menu(void){
+printf("==============================\n");
+printf("    SISTEMA EQUIPE TUCURUVI\n");
+printf("==============================\n");
 
 int main() {
-    
+   
 
     
     return 0;
