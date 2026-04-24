e654679873f3cf4703402c44b5d4111689f8640d
LSHummel <lshummel13@gmail.com>
2026-04-24 09:37:08 -0300

    menu até o 4

--- src/main.c
+++ src/main.c
@@ -3,6 +3,10 @@ void menu(void){
 printf("==============================\n");
 printf("    SISTEMA EQUIPE TUCURUVI\n");
 printf("==============================\n");
+printf("1 - Inserir notas\n");
+printf("2 - Calcular media\n");
+printf("3 - Verificar situacao\n");
+printf("4 - Exibir resultado\n");
 
 int main() {
    
